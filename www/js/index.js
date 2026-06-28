7a983e3c45a9767bd81cb94d06c39cb0fcec51fc
Rexi0227 <mcred0227@gmail.com>
2026-06-29 00:35:22 +0800

    set map zoomSnap, zoomDelta

--- www/js/index.js
+++ www/js/index.js
@@ -84,7 +84,7 @@ async function loadTownId() {
 }
 
 async function mapInit(mapid){
-    const map = L.map(mapid).setView([23.7, 120.924610], 8);
+    const map = L.map(mapid, {zoomSnap: 0.25, zoomDelta: 0.25}).setView([23.7, 120.924610], 8);
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', { 
         minZoom: 3, 
